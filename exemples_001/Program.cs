8a24a4c716f8e6256686ff939540a8b0a64bf8af
Eugen Eugen <zhenya@localhost>
2022-12-18 22:29:19 +0300

    add 0 task

--- exemples_001/Program.cs
+++ exemples_001/Program.cs
@@ -1,1 +1,10 @@
-﻿Console.WriteLine("Hello, World!");
+﻿//Напишите программу, которая на вход принимает
+// число и выдаёт его квадрат (число умноженное на само себя).
+//Например: 4->16
+//-3->9 -7->49
+
+int number = int.Parse(Console.ReadLine());
+
+int result = number * number;
+
+Console.WriteLine(result);
