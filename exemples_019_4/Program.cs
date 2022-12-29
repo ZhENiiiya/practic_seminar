f608aaf10797700db5e5826c3a1c9b8f6f16ba12
Eugen Eugen <zhenya@localhost>
2022-12-29 22:51:51 +0300

    upload exemples_019_4

---
+++ exemples_019_4/Program.cs
@@ -0,0 +1,29 @@
+﻿// Напишите программу, которая выводит массив из 8 элементов, заполненный нулями и единицами в случайном порядке.
+// [1,0,1,1,0,1,0,0]
+
+int[] matrix = new int[8];
+
+PrintArray(GetRandomMatrix(matrix));
+
+
+void PrintArray(int[] matr)
+{
+    Console.Write("[");
+    for (int i = 0; i < matr.Length-1; i++)
+    {
+        Console.Write($"{matr[i]}, ");
+        if (i == matr.Length-2) Console.Write($"{matr[i+1] }");
+    }
+    Console.Write("]");
+}
+
+int[] GetRandomMatrix(int[] matr)
+{
+    Random r = new Random();
+    for (int i = 0; i < matr.Length; i++)
+    {
+        
+        matr[i] = r.Next(0, 2);
+    }
+    return matr;
+}
