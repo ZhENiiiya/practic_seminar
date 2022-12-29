b96084f1610aa7cf07bba17643137bddb406e943
Eugen Eugen <zhenya@localhost>
2022-12-29 18:58:36 +0300

    add exemples_016_4 and new code common

--- Common/Class1.cs
+++ Common/Class1.cs
@@ -3,6 +3,19 @@
 
     public static class SystemCordinate
 {
+    public static int GetNumberFromUser()
+    {
+        bool isParsed = int.TryParse(Console.ReadLine(), out int number);
+
+        if(isParsed)
+        {
+            return number;
+        }
+        else
+        {
+            return GetNumberFromUser();
+        }
+    }
     public static int GetNumberOfQuarter(int x, int y)
     {
         if (x > 0 && y > 0) return 1;
---
+++ exemples_016_4/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая принимает на вход числа (A) и 
+// выдает сумму чисел от 1 до A.
+// 7 -> 28
+// 4 -> 10
+// 8 -> 36
+using Common;
+
+int a = SystemCordinate.GetNumberFromUser();
+
+int sum = GetSumNumbersBetweenOneToNumbers(a);
+
+Console.WriteLine(sum);
+
+int GetSumNumbersBetweenOneToNumbers(int number)
+{
+    int sum = 0;
+    for (var i = 1; i <= number; i++)
+    {
+        sum += i;
+    }
+    return sum;
+}
