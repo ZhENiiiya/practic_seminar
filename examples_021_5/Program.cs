a7004c13a572537538895930eb1f2ee688d3ff5d
Eugen Eugen <zhenya@localhost>
2023-01-03 11:30:40 +0300

    upload examples_021_5

---
+++ examples_021_5/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 32: Напишите программу замена элементов массива: 
+// положительные элементы замените на соответствующие отрицательные, 
+// и наоборот.
+// [-4, -8, 8, 2] -> [4, 8, -8, -2]
+
+int[] array = new int[]{-4, -8, 8, 2};
+
+PrintArray(array, ReplacingPositiveWithNegativeAndViceVersa(array));
+
+int[] ReplacingPositiveWithNegativeAndViceVersa(int[] array)
+{
+    int[] arrayResult = new int[array.Length];
+    for (int i = 0; i < array.Length; i++)
+    {
+        arrayResult[i] = -array[i];
+    }
+    return arrayResult;
+}
+
+void PrintArray(int[] array1, int[] array2)
+{
+    Console.WriteLine($"[{string.Join(", ", array1)}] - [{string.Join(", ", array2)}]");
+}
