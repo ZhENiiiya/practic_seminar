663de0e68e10c3b7df79d2e307d6002cccb8de41
Eugen Eugen <zhenya@localhost>
2023-01-04 19:43:07 +0300

    upload examples_027_6

---
+++ examples_027_6/Program.cs
@@ -0,0 +1,36 @@
+﻿// Напишите программу, которая будет 
+// преобразовывать десятичное число в двоичное. 
+// 45 -> 101101
+// 3 -> 11
+// 2 -> 10
+
+int numberDex = 45;
+
+Console.WriteLine($"{numberDex} -> {PrintArray(ConvertDexToBin(numberDex))}");
+
+int[] ConvertDexToBin(int num)
+{
+    int[] binCode;
+
+    int count = 0;
+    for (int i = num; i != 0; i /= 2)
+    {
+        count++;
+    }
+    binCode = new int[count];
+
+    for (int i = binCode.Length - 1; i >= 0; i--)
+    {
+        binCode[i] = num % 2;
+        num /= 2;
+    }
+
+    return binCode;
+}
+
+
+string PrintArray(int[] array)
+{
+    return($"{string.Join("", array)}");
+}
+
