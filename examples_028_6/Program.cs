b6076be603ac604df511040f79f12324f30a64f5
Eugen Eugen <zhenya@localhost>
2023-01-04 21:59:34 +0300

    upload file

--- examples_028_6/Program.cs
+++ examples_028_6/Program.cs
@@ -3,24 +3,24 @@
 // Если N = 5 -> 0 1 1 2 3 Если N = 3 -> 0 1 1
 // Если N = 7 -> 0 1 1 2 3 5 8
 
-int num = 10;
+int num = 2;
 Console.Write($"{num} -> ");
 PrintArray(Fibonacci(num));
 
-double[] Fibonacci(int n)
+int[] Fibonacci(int n)
 {
-    double[] fibonacciNum = new double[n];
-    fibonacciNum[0] = 0;
-    fibonacciNum[1] = 1;
+    int[] fibonacciNum = new int[n];
+    if (n > 0) fibonacciNum[0] = 0;
+    if (n > 1) fibonacciNum[1] = 1;
     for (int i = 2; i < fibonacciNum.Length; i++)
     {
         fibonacciNum[i] = fibonacciNum[i - 1] + fibonacciNum[i - 2];
-    
+
     }
     return fibonacciNum;
 }
 
-void PrintArray(double[] array)
+void PrintArray(int[] array)
 {
-    Console.WriteLine(string.Join(", ", array));
+    Console.WriteLine(string.Join(" ", array));
 }
