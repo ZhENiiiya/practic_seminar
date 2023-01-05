94b918266c608580f865c2ecc067948d462395e4
Eugen Eugen <zhenya@localhost>
2023-01-05 10:39:13 +0300

    upload examples_029_6

---
+++ examples_029_6/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите программу, которая будет создавать 
+// копию заданного массива с помощью
+// поэлементного копирования.
+
+int[] array = new int[]{1, 2, 4, 4, 5, 6};
+
+PrintArray(array);
+PrintArray(CopyArray(array));
+
+int[] CopyArray(int[] array)
+{
+    int[] arrayCopy = new int[array.Length];
+    for (int i = 0; i < array.Length; i++)
+    {
+        arrayCopy[i] = array[i];
+    }
+    return arrayCopy;
+}
+
+void PrintArray(int[] array)
+{
+    Console.WriteLine(string.Join(", ", array));
+}
