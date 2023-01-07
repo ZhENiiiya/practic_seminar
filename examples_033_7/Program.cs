0f5713c4a7b54bfbf47d729de8c96723bce9e0c3
Eugen Eugen <zhenya@localhost>
2023-01-08 00:04:30 +0300

    upload examples_033_7

---
+++ examples_033_7/Program.cs
@@ -0,0 +1,44 @@
+﻿// Задайте двумерный массив. Найдите сумму
+// элементов, находящихся на главной диагонали (с индексами
+// (0,0); (1;1) и т.д.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Сумма элементов главной диагонали: 1+9+2 = 12
+
+int[,] array2D = new int[,] { { 1, 4, 7, 2 },
+                              { 5, 9, 2, 3 },
+                              { 8, 4, 2, 4 } };
+
+Print2DArray(array2D);
+int res = SumDiagonal(array2D);
+Console.WriteLine(res);
+
+int SumDiagonal(int[,] array)
+{
+    int result = 0;
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            if (i == j) result += array[i, j];
+        }
+    }
+    return result;
+}
+
+void Print2DArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+
+
