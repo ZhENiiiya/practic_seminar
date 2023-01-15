91f7a6d22c18a573cb3a95b4ae4da35e060d2d68
Eugen Eugen <zhenya@localhost>
2023-01-15 14:52:15 +0300

    upload examples_038_9

---
+++ examples_038_9/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задайте значение N. 
+// Напишите программу, которая выведет все 
+// натуральные числа в промежутке от 1 до N.
+// N = 5 -> "1, 2, 3, 4, 5"
+// N = 6 -> "1, 2, 3, 4, 5, 6"
+
+int n = 5;
+
+PrintNum(n, 1);
+
+void PrintNum(int n, int counter)
+{
+    if (n == 0)
+    {}
+    else
+    {
+        Console.WriteLine(counter);
+        PrintNum(n-1,counter+1);
+    }
+}
