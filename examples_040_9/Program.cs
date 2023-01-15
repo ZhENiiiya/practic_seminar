756f1f4e842d05d97529f316222f18145d1cf1d5
Eugen Eugen <zhenya@localhost>
2023-01-15 15:34:45 +0300

    upload examples_040_9

---
+++ examples_040_9/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите программу, 
+// которая будет принимать на вход число и 
+// возвращать сумму его цифр.
+// 453 -> 12 45 -> 9
+int x = 453;
+
+Console.WriteLine(GetSumNumber(x));
+
+int GetSumNumber( int num)
+{
+    int res = 0;
+    if (num > 0)
+    {
+        res += num % 10;
+        num = num / 10;
+    }
+    else
+    {
+        return res;
+    }
+    return res + GetSumNumber(num);
+    
+}
